2d6ea8c65bfa7e43167beb830ca9b2c36d297ef8
Adhemir Sabino <adhemirsabino@gmail.com>
2023-05-08 18:22:49 -0500

    database connection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { httpServer, app } = require("./src/app.js");
-const { conn } = require("./src/db.js");
+const { conn } = require("./src/sequelize/sequelize");
 
-conn.sync({ force: true });
+conn.sync({ force: false });
 httpServer.listen(
   app.get("port"),
   console.log(`Server running on: http://localhost:${app.get("port")}`)
