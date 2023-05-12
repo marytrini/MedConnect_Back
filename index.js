137d6105cb1a055f6ae31892d8dda581a93855c8
Adhemir Sabino <adhemirsabino@gmail.com>
2023-05-12 12:15:45 -0500

    force: false

--- index.js
+++ index.js
@@ -3,23 +3,19 @@ const { conn, Administrator } = require("./src/sequelize/sequelize");
 
 app.set("port", process.env.PORT || 4000);
 
-conn.sync({ force: true }).then(async function () {
-
-  await Administrator.create(
-    {
-      email: "admin@medconnect.com",
-      password: "Aezakmi11",
-      firstName: "MedConnect",
-      lastName: "Owner",
-      phone: "+12111216565",
-      role: "master",
-      userType: "admin"
-    })
+conn.sync({ force: false }).then(async function () {
+  await Administrator.create({
+    email: "admin@medconnect.com",
+    password: "Aezakmi11",
+    firstName: "MedConnect",
+    lastName: "Owner",
+    phone: "+12111216565",
+    role: "master",
+    userType: "admin",
+  });
 
   httpServer.listen(
     app.get("port"),
     console.log(`Server running on: http://localhost:${app.get("port")}`)
   );
-
-})
-
+});
