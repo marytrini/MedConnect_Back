e53eefe81cfbbca4f74b398731130d3027a94b45
maritrini <mariatrinidadsalazarr@gmail.com>
2023-05-16 11:18:03 -0500

    ruta médicos query

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,2 +1,24 @@
 const { Appointment, Medico, Patient } = require("../sequelize/sequelize");
 const { handleHttpError } = require("../utils/handleError");
+
+const getAppointment = async () => {
+  try {
+    const citas = await Appointment.findAll({
+      include: [
+        {
+          model: Medico,
+          attributes: ["first_name", "last_name"],
+        },
+        {
+          model: Patient,
+          attributes: ["firstName", "lastName"],
+        },
+      ],
+    });
+    citas.length
+      ? res.status(200).json(citas)
+      : handleHttpError(res, "No existe la cita", 404);
+  } catch (error) {
+    handleHttpError(res, { error: error.message }, 500);
+  }
+};
