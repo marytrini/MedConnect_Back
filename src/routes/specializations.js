d1a3c1939564637fe04aa94f31c165c37b1cf728
maritrini <mariatrinidadsalazarr@gmail.com>
2023-05-22 13:33:23 -0500

    ruta put en specialization

--- src/routes/specializations.js
+++ src/routes/specializations.js
@@ -6,10 +6,12 @@ const {
   getSpecializations,
   deleteSpecializations,
   getSpecialization,
+  updateSpecialization,
 } = require("../controllers/specializationController");
 
 router.get("/", getSpecializations);
 router.get("/:id", getSpecialization);
 router.post("/", createSpecializations);
+router.put("/:id", updateSpecialization);
 router.delete("/:id", deleteSpecializations);
 module.exports = router;
