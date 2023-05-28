9f31d8582d02c9d717de2994a95cf3d06e8dcb7c
Adhemir Sabino <adhemirsabino@gmail.com>
2023-05-28 13:34:45 -0500

    cors2

--- src/app.js
+++ src/app.js
@@ -12,11 +12,23 @@ const { router } = require("./routes/router.js");
 const app = express();
 const httpServer = createServer(app);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use(
   cors({
-    origin: ["https://med-connect-front.vercel.app", "http://localhost:3000"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
+    origin: [
+      "http://localhost:3000",
+      "https://medconnectback-production.up.railway.app",
+    ],
     credentials: true,
   })
 );
