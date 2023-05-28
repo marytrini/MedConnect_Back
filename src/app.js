b2106614121812393dc946ade3de70c59276d28b
maritrini <mariatrinidadsalazarr@gmail.com>
2023-05-28 12:29:21 -0500

    remove s

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.use(require("morgan")("dev"));
 // );
 app.use(
   require("cors")({
-    origin: "https://localhost:3000",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
