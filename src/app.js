9874ccb5a4749008d69ea49af74165af68704149
Adhemir Sabino <adhemirsabino@gmail.com>
2023-05-30 16:42:21 -0500

    cambio de dominio

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const httpServer = createServer(app);
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://med-connect-front.vercel.app"
+    "https://med-develop.vercel.app/"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader(
