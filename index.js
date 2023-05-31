da063ec570de6ca46f64328460e000c41ffd8417
maritrini <mariatrinidadsalazarr@gmail.com>
2023-05-31 17:56:43 -0500

    index

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 const { httpServer, app } = require("./src/app.js");
-const { conn, Administrator } = require("./src/sequelize/sequelize");
+const { conn } = require("./src/sequelize/sequelize");
 
 app.set("port", process.env.PORT || 4000);
 
-// conn.sync({ force: false }).then(async function () {
-//   // await Administrator.create({
-//   //   email: "admin@medconnect.com",
-//   //   password: "Aezakmi11",
-//   //   firstName: "MedConnect",
-//   //   lastName: "Owner",
-//   //   phone: "+12111216565",
-//   //   role: "master",
-//   //   userType: "admin",
-//   // });
-
-// });
 conn.sync({ force: false });
 httpServer.listen(
   app.get("port"),
