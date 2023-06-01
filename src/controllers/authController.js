f6ebd28fa1860d066a0603b5673f798d071e64be
maritrini <mariatrinidadsalazarr@gmail.com>
2023-05-31 23:35:00 -0500

    https

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,7 +5,7 @@ const { encrypt, compare } = require("../utils/handlePassword");
 const { handleHttpError } = require("../utils/handleError");
 const transporter = require("../config/mailer");
 const { USER_EMAIL } = process.env;
-const CLIENT_URL = "medconnectfront-production.up.railway.app";
+const CLIENT_URL = "https://medconnectfront-production.up.railway.app";
 
 const registerCtrl = async (req, res) => {
   try {
